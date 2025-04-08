41218c19919d81fc79f8c026b1d9e11814ecacff
george-josep <georgejoseph2027@mca.ajce.in>
2025-04-08 11:51:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    with open("home.html", "r", encoding="utf-8") as f:
+        html_content = f.read()
+    return render_template_string(html_content)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
