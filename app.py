d97d00c30ed9a4f1350ff0b5becf7702f0d4cde7
george-josep <georgejoseph2027@mca.ajce.in>
2025-04-08 11:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to MatrimoSys!"
-
-if __name__ == "__main__":
-    # The host "0.0.0.0" allows the app to be accessible from outside the container.
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    return render_template("home.html")
