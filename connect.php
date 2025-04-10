58c317f9f1411163acbd2c23c12d27ea08d157b0
george-josep <georgejoseph2027@mca.ajce.in>
2025-04-10 09:21:05 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Database credentials
 $servername = "localhost"; // Usually localhost if using a local server
 $username = "root";        // MySQL username (default is root)
 $password = "";            // MySQL password (default is empty)
@@ -12,3 +17,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
